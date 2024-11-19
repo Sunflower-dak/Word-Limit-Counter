ecf986a92fc7e2bf3d64c1e04ad656720257cc9a
Kwaku Asetena-Krah <kwakukrah.da@gmail.com>
2024-11-19 17:38:06 +0000

    commit

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -4,29 +4,30 @@ import Warning from "./Warning";
 export default function TextArea() {
   const [text, setText] = useState("");
   const [alertWarning, setAlertWarning] = useState(false);
-  const [warningMessage, setWarningMessage] = useState('')
+  const [warningMessage, setWarningMessage] = useState("");
 
   const textHandler = (e) => {
     let newText = e.target.value;
     if (newText.includes("fuck")) {
       setAlertWarning(true);
       newText = newText.replace("fuck", "");
-    } else{
+    } else {
       if (newText.includes("@")) {
         setAlertWarning(true);
         newText = newText.replace("@", "");
+      }
+      setText(newText);
     }
-    setText(newText);
-  };
 
-  return (
-    <div className="textarea">
-      <textarea
-        placeholder="Write here to know your word count..."
-        value={text}
-        onChange={textHandler}
-      />
-      {alertWarning ? <Warning /> : null}
-    </div>
-  );
+    return (
+      <div className="textarea">
+        <textarea
+          placeholder="Write here to know your word count..."
+          value={text}
+          onChange={textHandler}
+        />
+        {alertWarning ? <Warning /> : null}
+      </div>
+    );
+  };
 }
